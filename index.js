6cc6025bb22b898ee87acec72de925fbd4ca23c9
Zemmouri Farouk <faroukwebdesign@gmail.com>
2023-07-22 12:17:08 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+const multiply = num1*num2
+console.log(multiply)
+const random = Math.floor(Math.random() * 100) + 1
+let num3 = 29
+let num4 = 5
+const mod = num3%num4
+console.log(mod)
+const max = Math.max(5, 10, 15, 20)
